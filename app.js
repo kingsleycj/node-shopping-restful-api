c5aeef36725a99cab632a95d1483e3419594a06f
kingsleycj <primekings.kc@gmail.com>
2023-02-24 03:23:40 +0100

    handling errors and CORS

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express(); 
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
 
 const productRoutes = require('./src/routes/products');
 const orderRoutes = require("./src/routes/orders");
 
 
 app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header( 'Access-Control-Allow-Origin' , '*' );
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.header("Access-Control-Allow-Methods", 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+    next();
+});
+
 // Routes that should handle requests
 app.use('/products', productRoutes);
 app.use("/orders", orderRoutes);
