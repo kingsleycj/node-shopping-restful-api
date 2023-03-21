8082ea9e5de7c01e264973403382f838c0e3b5db
kingsleycj <primekings.kc@gmail.com>
2023-03-21 02:59:42 +0100

    check auth removed for debugging purposees

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const checkAuth = require("../middleware/check.auth")
+
 const userController = require("../controllers/user")
 
 // create a new user
