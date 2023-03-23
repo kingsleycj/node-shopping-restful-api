7b0a3030094311aad12079f40f2f41d3fb916226
kingsleycj <primekings.kc@gmail.com>
2023-03-23 01:21:41 +0100

    check auth removed for debugging purposees

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require('../controllers/orders');
-// const checkAuth = require('../middleware/check.auth');
 
 // Handle incoming GET requests  to /orders
 router.get("/", orderController.getAllOrders);
